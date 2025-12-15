12e253547d2dfb28ee8babf77be03c6a3563d57c
Will Forcey <wsforc3y@gmail.com>
2025-12-14 19:22:05 -0500

    need to fix bug

--- main.go
+++ main.go
@@ -9,17 +9,18 @@ import "C"
 import (
 	"fmt"
 	"math"
+	"math/cmplx"
 	"os"
 	"runtime"
 	"strconv"
 	"time"
 )
 
-var poles []complex128
-var zeros []complex128
+var inputPoles []complex128
+var inputZeros []complex128
 var numCPU int = runtime.NumCPU()
 
-func getInputLoopTransferFunction(poles, zeros []complex128) {
+func getInputLoopTransferFunction(poles, zeros *[]complex128) {
 	// get input poles and zeroes of the loop transfer function
 	if len(os.Args) <= 1 {
 		fmt.Println("ERROR: input args in format e.g.: p 0.5 1+2i (1-2i) z 1i 2 -5")
@@ -54,22 +55,52 @@ func getInputLoopTransferFunction(poles, zeros []complex128) {
 
 		// else must have gotten a correct number format so store in respective slices
 		if p {
-			poles = append(poles, cmplx)
+			*poles = append(*poles, cmplx)
+			fmt.Println(*poles)
+			fmt.Println(inputPoles)
 		} else {
-			zeros = append(zeros, cmplx)
+			*zeros = append(*zeros, cmplx)
 		}
 	}
 }
 
 func drawPhasePlot(poles, zeros []complex128) {
-	for j := 0; j < 200; j++ {
-		for i := float64(0); i < 1; i += 0.001 {
-			p := 2 * math.Pi * i
-			C.DrawPixel(C.int(500/2+float64(j)*math.Cos(p)), C.int(500/2+float64(j)*-math.Sin(p)), C.int(colorFromPhase(p)))
+	// for j := 0; j < 200; j++ {
+	// 	for i := float64(0); i < 1; i += 0.001 {
+	// 		p := 2 * math.Pi * i
+	// 		C.DrawPixel(C.int(500/2+float64(j)*math.Cos(p)), C.int(500/2+float64(j)*-math.Sin(p)), C.int(colorFromPhase(p)))
 
-			// fmt.Println(C.int(500/2 + 10*math.Cos(p)))
+	// 		// fmt.Println(C.int(500/2 + 10*math.Cos(p)))
+	// 	}
+	// }
+
+	// get phase by plugging every point in plane into the loop transfer function
+	// most plots in z-domain we are interested in unit circle area
+	// so will limit the bounds of the plane for now to -2 to 2
+	const windowWidth = 500
+	const windowHeight = 500
+	for i := float64(-2); i < 2; i += windowWidth / 4 {
+		for j := float64(-2); j < 2; j += windowHeight / 4 {
+			p := math.Pi + cmplx.Phase(loopTransferFunction(complex(i, j), poles, zeros))
+			C.DrawPixel(C.int(windowWidth/2+(i/2)*windowWidth/2), C.int(windowHeight/2+(j/2)*windowHeight/2), C.int(colorFromPhase(p)))
+			fmt.Println(windowWidth/2 + (i/2)*windowWidth/2)
 		}
 	}
+}
+
+// evaluate a loop transfer function at a test point zm
+func loopTransferFunction(zm complex128, poles, zeros []complex128) complex128 {
+	var y complex128 = 1
+
+	for _, z := range zeros {
+		y *= zm - z
+	}
+
+	for _, p := range poles {
+		y /= zm - p
+	}
+
+	return y
 }
 
 // p: input phase in radians from 0 to 2pi
@@ -82,8 +113,7 @@ func colorFromPhase(p float64) int {
 	// p = math.Mod(p, 2*math.Pi)
 	var r, g, b int
 
-	// 7 intervals
-	// https://www.rapidtables.com/convert/color/hsv-to-rgb.html
+	// 6 intervals to get color from (RGB)
 	switch {
 	case p <= 2*math.Pi/6:
 		g = int(p / (2 * math.Pi / 6) * 255)
@@ -109,6 +139,9 @@ func colorFromPhase(p float64) int {
 }
 
 func main() {
+	// user should input poles and zeros as command arguments when running program
+	getInputLoopTransferFunction(&inputPoles, &inputZeros)
+
 	go func() {
 		for {
 			time.Sleep(time.Millisecond)
@@ -119,22 +152,19 @@ func main() {
 
 		fmt.Println("Number of CPU Logical Processors: ", numCPU)
 
-		fmt.Printf("%X\n", C.int(colorFromPhase(0.5*math.Pi/3)))
-		fmt.Printf("%X\n", colorFromPhase(2*math.Pi/3))
-		fmt.Printf("%X\n", colorFromPhase(3*math.Pi/3))
-		fmt.Printf("%X\n", colorFromPhase(4*math.Pi/3))
-		fmt.Printf("%X\n", colorFromPhase(5*math.Pi/3))
-		fmt.Printf("%X\n", colorFromPhase(6*math.Pi/3))
+		// fmt.Printf("%X\n", C.int(colorFromPhase(0.5*math.Pi/3)))
+		// fmt.Printf("%X\n", colorFromPhase(2*math.Pi/3))
+		// fmt.Printf("%X\n", colorFromPhase(3*math.Pi/3))
+		// fmt.Printf("%X\n", colorFromPhase(4*math.Pi/3))
+		// fmt.Printf("%X\n", colorFromPhase(5*math.Pi/3))
+		// fmt.Printf("%X\n", colorFromPhase(6*math.Pi/3))
 
-		// user should input poles and zeros as command arguments when running program
-		getInputLoopTransferFunction(poles, zeros)
-
-		fmt.Println("Poles: ", poles)
-		fmt.Println("Zeros: ", zeros)
+		fmt.Println("Poles: ", inputPoles)
+		fmt.Println("Zeros: ", inputZeros)
 
 		// for {
 		// C.DrawPixel(5, 5, 0xFF00FF)
-		drawPhasePlot(poles, zeros)
+		drawPhasePlot(inputPoles, inputZeros)
 		// for i := 0; i < 500; i++ {
 		// 	for j := 0; j < 500; j++ {
 		// 		C.DrawPixel(C.int(i), C.int(j), C.int(colorFromPhase(float64(i)/500*2*math.Pi)))
